a97426174b8b14da89f9c040769813b745cb9f1b
allstons <jagschwartz@aol.com>
2015-02-18 21:56:08 -0500

    the files originally put in first repo, Chx-Run

---
+++ main.js
@@ -0,0 +1,24 @@
+confirm("Are you ready to play?");
+
+confirm("I am ready! Cluck! Cluck");
+
+varAge=prompt("Are you a spry chick or an aged rubber chicken?");
+if (age<20) {
+  console.log("Let's get flappin'!");
+} else {
+  console.log("Good luck!");
+};
+
+console.log("Farmer Ted has enjoyed some of his wife, Hanna's moonshine, and now he is plowing the field. Over yonder comes his brother, Nugget Ned, known to have the best Chicken Shack in town. In fact, some of his tastiest chickens have been your friends! You don't want to be next. What do you do?");
+
+
+
+console.log("Do you hide in the dark corner, squeeze through the loose board in the back of the chicken coup and make a dash for the Chicken Reserve across the field?");
+
+var userAnswer = prompt("Do you make a run for it?");
+
+if (userAnswer === "Run for it!") {
+  console.log("Go! Go! Go! Watch out for Farmer Ted and the tractor!");
+} else {
+  console.log("You stayed in, huh? Let's see if Nugget Ned finds another chicken.");
+  };
